a1109a8bad285e3760eb70f38be989ba93f544a9
Eric Martin <coelomate@gmail.com>
2017-10-27 19:35:05 -0400

    Updated service worker issue
    Was causing caching and routing issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// the below code was causing caching and routing issues, so removed it
+// and the import above. If mystery issues occur or you want the performance,
+// you can add it back. 
+
+// registerServiceWorker();
